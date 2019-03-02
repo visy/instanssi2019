0a557b7bbdfe12acf50bf2cc3f85b57f9d8cf76e
unconditionalbranch <sampo.savilampi@gmail.com>
2019-03-02 06:33:50 +0200

    Update Demo.cpp
    3 effects ~~finished

--- instanssi2019/Demo.cpp
+++ instanssi2019/Demo.cpp
@@ -10,6 +10,15 @@
 
 #include "sync.h"
 
+struct Vector4 {
+	float x, y, z, w;
+};
+struct Vector3 {
+	float x, y, z;
+};
+struct Vector2 {
+	float x, y;
+};
 
 static const float bpm = 150.0f; /* beats per minute */
 static const int rpb = 8; /* rows per beat */
@@ -42,7 +51,7 @@ SDL_Surface *paita_image;
 
 SDL_Rect font_bb[15 * 16] = {};
 
-unsigned char house_mask[320 * 100] = {255};
+unsigned char house_mask[320 * 100] = { 255 };
 
 float plx;
 float ply;
@@ -177,9 +186,9 @@ void VertLine(int x, int y1, int y2, Uint32 color, float z) {
 
 	int ys = y2;
 	while (ys > y1) {
-		ys-=2;
+		ys -= 2;
 		pixels[ys * effu_w + x] = (*cc);
-		pixels[(ys-1) * effu_w + x] = (*cc);
+		pixels[(ys - 1) * effu_w + x] = (*cc);
 	}
 
 }
@@ -218,6 +227,41 @@ Uint8 luma(Uint8 r, Uint8 g, Uint8 b) {
 	return (Uint8)(0.2126*r + 0.7152*g + 0.0722*b);
 }
 
+Uint32 get_pixel(SDL_Surface *surface, int x, int y)
+{
+	int bpp = surface->format->BytesPerPixel;
+	/* Here p is the address to the pixel we want to retrieve */
+	Uint8 *p = (Uint8 *)surface->pixels + y * surface->pitch + x * bpp;
+
+	switch (bpp) {
+	case 1:
+		return *p;
+		break;
+
+	case 2:
+		return *(Uint16 *)p;
+		break;
+
+	case 3:
+		if (SDL_BYTEORDER == SDL_BIG_ENDIAN)
+			return p[0] << 16 | p[1] << 8 | p[2];
+		else
+			return p[0] | p[1] << 8 | p[2] << 16;
+		break;
+
+	case 4:
+		return *(Uint32 *)p;
+		break;
+
+	default:
+		return 0;       /* shouldn't happen, but avoids warnings */
+	}
+}
+void set_pixel(SDL_Renderer *rend, int x, int y, Uint8 r, Uint8 g, Uint8 b, Uint8 a)
+{
+	SDL_SetRenderDrawColor(rend, r, g, b, a);
+	SDL_RenderDrawPoint(rend, x, y);
+}
 
 void DoHeightmap(float px, float py, float angle, float h, float horizon, float scale_h, float distance, int iter) {
 	int sw = effu_w;
@@ -234,7 +278,7 @@ void DoHeightmap(float px, float py, float angle, float h, float horizon, float 
 	float z = 1;
 	plx = 0;
 	ply = 0;
-	int o = cos(time*0.0001)*150;
+	int o = cos(time*0.0001) * 150;
 	while (z < distance) {
 		plx = (-cos_a * z - sin_a * z) + px;
 		ply = (sin_a*z - cos_a * z) + py;
@@ -245,7 +289,7 @@ void DoHeightmap(float px, float py, float angle, float h, float horizon, float 
 		float dx = (prx - plx) / sw;
 		float dy = (pry - ply) / sw;
 
-		for (int i = 0; i < sw; i+=1) {
+		for (int i = 0; i < sw; i += 1) {
 			int c_plx = (int)plx & 1023;
 			int c_ply = (int)ply & 1023;
 			int offset = (c_ply << 10) + c_plx;
@@ -253,7 +297,7 @@ void DoHeightmap(float px, float py, float angle, float h, float horizon, float 
 
 			int height_on_screen = (int)(((h  * cos(sin(z*0.01))) - colormap_buffer[offset]) / z * scale_h + horizon);
 			height_on_screen *= 2;
-			VertLine(i, (int)height_on_screen+effu_h, ybuffer[i], heightmap_buffer[offset], z);
+			VertLine(i, (int)height_on_screen + effu_h, ybuffer[i], heightmap_buffer[offset], z);
 
 
 			plx += dx;
@@ -302,8 +346,8 @@ void DoText(const char* text, int x, int y) {
 		SDL_Rect srcrect = font_bb[(font_y * 16) + font_x];
 
 		SDL_Rect dstrect;
-		dstrect.x = cursor_x+off;
-		dstrect.y = cursor_y+off;
+		dstrect.x = cursor_x + off;
+		dstrect.y = cursor_y + off;
 		dstrect.w = srcrect.w * 2;
 		dstrect.h = srcrect.h * 2;
 		SDL_RenderCopy(ren, font_texture, &srcrect, &dstrect);
@@ -318,11 +362,11 @@ void DoFont() {
 
 	int ti = (int)sync_c_a;
 
-	if (ti == 1) DoText("______Quadtrip+Ivory+Jumalauta______", 100, 1080/2-32);
+	if (ti == 1) DoText("______Quadtrip+Ivory+Jumalauta______", 100, 1080 / 2 - 32);
 	if (ti == 2) DoText("PURGATORIUm____________", 100, 1080 / 2 - 32);
 
 
-//	if (ti == 1) DoText("performing_forbidden_rituals_outside", 100, 100);
+	//	if (ti == 1) DoText("performing_forbidden_rituals_outside", 100, 100);
 }
 
 void RenderHeightmap() {
@@ -334,23 +378,218 @@ void RenderHeightmap() {
 	DoHeightmap(sync_x, sync_y, sync_rot, sync_h, sync_hori, sync_scaleh, sync_dist, 0);
 }
 
+SDL_Surface *sshot;
 void RenderKefrensCross() {
 
 	SDL_Rect dstrect;
 
 	SDL_SetRenderTarget(ren, scrtexture);
-	SDL_SetTextureBlendMode(scrtexture,SDL_BLENDMODE_BLEND);
+	SDL_SetTextureBlendMode(scrtexture, SDL_BLENDMODE_BLEND);
 	SDL_SetRenderDrawColor(ren, sync_c_r, sync_c_g, sync_c_b, SDL_ALPHA_OPAQUE);
 	SDL_RenderClear(ren);
-	for (int i = 0; i < 300; i++) {
-		dstrect.x = 640 / 2 - 64 + 100 * sin(sync_dist + 2.0f *  3.141592f * i / 100) * cos(sync_rot + 2.0f *  3.141592f * i / 128);
-		dstrect.y = 480 / 2 - 64 + i - 150;
-		dstrect.w = 128;
-		dstrect.h = 128;
+	dstrect.w = 128;
+	dstrect.h = 128;
+	for (int i = 0; i < 100; i += 2) {
+		dstrect.x = 320 / 2 - 64 + 100 * sin(sync_dist + 2.0f *  3.141592f * i / 100) * cos(sync_rot + 2.0f *  3.141592f * i / 128);
+		dstrect.y = 200 / 2 - 64 + i - 80 + sin(sync_dist*0.321f + i * 0.05f) * 30;
 		SDL_RenderCopy(ren, reversecross_texture, NULL, &dstrect);
+	}
+	int w = 1920;
+	int h = 1080;
+	if (sshot == NULL)
+		sshot = SDL_CreateRGBSurface(0, w, h, 32, 0x00ff0000, 0x0000ff00, 0x000000ff, 0xff000000);
+
+	SDL_RenderReadPixels(ren, NULL, SDL_PIXELFORMAT_ARGB8888, sshot->pixels, sshot->pitch);
+
+	for (int x = 16; x < 320 - 16; x++) {
+		auto selected = GetPixel(sshot, x, 0);
+		for (int y = 0; y < 200; y++) {
+			auto pixel = GetPixel(sshot, x, y);
+			Uint8 r, g, b, a;
+			SDL_GetRGBA(pixel, sshot->format, &r, &g, &b, &a);
+			if (pixel & 0x00ffffff > 0) {
+				selected = pixel;
+			}
+			else {
+				Uint8 r, g, b;
+				SDL_GetRGB(selected, sshot->format, &r, &g, &b);
+				set_pixel(ren, x, y, selected >> 16 & 255, selected >> 8 & 255, selected & 255, 255);
+			}
+			if (selected & 0x00ffffff == 0) { y += 10;  continue; }
+			Uint32 rr = selected >> 16 & 255;
+			Uint32 gg = selected >> 8 & 255;
+			Uint32 bb = selected & 255;
+			Uint32 aa = selected >> 24 & 255;
+			if (rr > 0) rr--;
+			if (gg > 0) gg--;
+			if (bb > 0) bb--;
+			selected = rr << 16;
+			selected |= gg << 8;
+			selected |= bb;
+			selected |= aa << 24;
+		}
+
+	}
+	set_pixel(ren, 100, 100, 32, 123, 200, 32);
+	SDL_SetRenderTarget(ren, NULL);
+	SDL_RenderPresent(ren);
+	//SDL_FreeSurface(sshot);
+}
+
+float dot(Vector3 A, Vector3 B)
+{
+	float product = 0;
+	product = product + A.x * B.x;
+	product = product + A.y * B.y;
+	product = product + A.z * B.z;
+	return product;
+}
+float step(float a, float b) {
+	if (b > a) return 1.0f;
+	return 0.0f;
+}
+Vector3 normalize(Vector3 v) {
+	float length = sqrt(v.x*v.x + v.y*v.y + v.z*v.z);
+	v.x /= length;
+	v.y /= length;
+	v.z /= length;
+	return v;
+}
+float sphere(Vector3 ray, Vector3 dir, Vector3 center, float radius)
+{
+	Vector3 rc; // = ray - center;
+	rc.x = ray.x - center.x;
+	rc.y = ray.y - center.y;
+	rc.z = ray.z - center.z;
+	float c = dot(rc, rc) - (radius*radius);
+	float b = dot(dir, rc);
+	float d = b * b - c;
+	float t = -b - sqrt(abs(d));
+	float st = 1.0f - step(0.0, min(t, d));
+	return  t + st;
+}
+Vector4 mainImage(Vector2 fragCoord, Vector2 iResolution)
+{
+	Vector4 C; // = vec4(1.0);
+	C = { 0.0f,0.0f,0.0f,0.0f };
+	Vector2 uv; // = fragCoord.xy / iResolution.xy - vec2(0.5);
+	uv.x = fragCoord.x / iResolution.x - 0.5f;
+	uv.y = fragCoord.y / iResolution.y - 0.5f;
+	float aspect = iResolution.x / iResolution.y;
+	Vector3 direction; // = normalize(vec3(.5 * uv * vec2(aspect, 1.0), 0.7));
+	direction.x = 0.5 * uv.x * aspect;
+	direction.y = 0.5 * uv.y;
+	direction.z = 0.7;
+	direction = normalize(direction);
+	float dist = sphere(Vector3{ 0.0f, 0.0f, -3.0f }, direction, Vector3{ 0.0f,0.0f,0.0f }, sync_h);
+	//C.rgb *= direction * dist*dist / 4.;
+	float d = dist * dist / 4.0f;
+	C.x = direction.x * d;
+	C.y = direction.y * d;
+	C.z = direction.z * d;
+	//C = texture(iChannel0, C.rg);
+	return C;
+}
+int flick = 0;
+void SphereEffect() {
+
+	SDL_Rect dstrect;
+
+	SDL_SetRenderTarget(ren, scrtexture);
+	SDL_SetTextureBlendMode(scrtexture, SDL_BLENDMODE_BLEND);
+	SDL_SetRenderDrawColor(ren, sync_c_r, sync_c_g, sync_c_b, SDL_ALPHA_OPAQUE);
+	float ax = sin(sync_x) * 256.0f;
+	float ay = cos(sync_y) * 256.0f;
+	flick++;
+	for (int y = 0; y < 200; y++) {
+		for (int x = 0; x < 320; x ++) {
+			Vector4 uv = mainImage(Vector2{ (float)x + sin(sync_rot)*100.0f,(float)y + cos(sync_rot*0.8f)*100.0f }, Vector2{ 320.0f,200.0f });
+			/*
+			auto pixel = GetPixel(sshot, x, y);
+			Uint8 r, g, b, a;
+			SDL_GetRGBA(pixel, sshot->format, &r, &g, &b, &a);
+			*/
+			Uint8 r, g, b, a;
+			r = (Uint8)(uv.x * 256.0f + ax) & 127;
+			g = (Uint8)(uv.y * 256.0f + ay) & 127;
+			b = (Uint8)(uv.z * 256.0f) & 255;
+			int d = uv.z * 128.0f;
+			int d2 = uv.z * 133.0f;
+			int d3 = uv.z * 144.0f;
+			auto pixel = GetPixel(reversecross_image, r, g);
+
+			r = pixel >> 16 & 255;
+			g = pixel >> 8 & 255;
+			b = pixel & 255;
+
+			r = (Uint8)max((int)r - d3, 0);
+			g = (Uint8)max((int)g - d2, 0);
+			b = (Uint8)max((int)b - d, 0);
+			a = pixel >> 24 & 255;
+
+			set_pixel(ren, x, y, r, g, b, 255);
+		}
 	}
 	SDL_SetRenderTarget(ren, NULL);
 	SDL_RenderPresent(ren);
+	//SDL_FreeSurface(sshot);
+}
+
+
+void FireEffect() {
+
+	SDL_Rect dstrect;
+
+	SDL_SetRenderTarget(ren, scrtexture);
+	SDL_SetTextureBlendMode(scrtexture, SDL_BLENDMODE_BLEND);
+	SDL_SetRenderDrawColor(ren, sync_c_r, sync_c_g, sync_c_b, SDL_ALPHA_OPAQUE);
+	int w = 1920;
+	int h = 1080;
+	if (sshot == NULL)
+		sshot = SDL_CreateRGBSurface(0, w, h, 32, 0x00ff0000, 0x0000ff00, 0x000000ff, 0xff000000);
+
+		for (int x = 0; x < 320; x++) {
+
+			Uint8 r, g, b, a;
+			r = (Uint8)rand();
+			g = (Uint8)rand() >> 2;
+			b = (Uint8)rand() >> 3;
+			if (rand() & 3 == 0) {
+				r = 255;
+				g = 255;
+				b = 255;
+			}
+			set_pixel(ren, x, 199, r, g, b, 255);
+		}
+	
+	SDL_RenderReadPixels(ren, NULL, SDL_PIXELFORMAT_ARGB8888, sshot->pixels, sshot->pitch);
+
+	flick++;
+	for (int y = 0; y < 199; y++) {
+		for (int x = flick & 1; x < 319; x+=2) {
+			/*
+			auto pixel = GetPixel(sshot, x, y);
+			Uint8 r, g, b, a;
+			SDL_GetRGBA(pixel, sshot->format, &r, &g, &b, &a);
+			*/
+			Uint8 r, g, b, a;
+			auto pixel = GetPixel(sshot, x + 1,min(y+ (rand() & 1 + 1),199) );
+
+			r = pixel >> 16 & 255;
+			g = pixel >> 8 & 255;
+			b = pixel & 255;
+
+			r = (Uint8)max((int)r - 2, 0);
+			g = (Uint8)max((int)g - 4, 0);
+			b = (Uint8)max((int)b - 8, 0);
+			a = pixel >> 24 & 255;
+
+			set_pixel(ren, x, y, r, g, b, 255);
+		}
+	}
+	SDL_SetRenderTarget(ren, NULL);
+	SDL_RenderPresent(ren);
+	//SDL_FreeSurface(sshot);
 }
 
 void RenderHouse() {
@@ -360,10 +599,10 @@ void RenderHouse() {
 
 	for (int y = 0; y < 100; y++) {
 		for (int x = 1; x < 320; x++) {
-			int xs = x * 1+((effu_w-2)/32);
+			int xs = x * 1 + ((effu_w - 2) / 32);
 			int ys = y * 3;
-			pixels[ys * effu_w + xs] = house_mask[y * 320 + x]*0x00111111;
-			pixels[(ys+1) * effu_w + xs] = house_mask[y * 320 + x] * 0x00111111;
+			pixels[ys * effu_w + xs] = house_mask[y * 320 + x] * 0x00111111;
+			pixels[(ys + 1) * effu_w + xs] = house_mask[y * 320 + x] * 0x00111111;
 		}
 	}
 
@@ -377,8 +616,8 @@ void RenderHouse() {
 
 	dstrect.x = effu_w / 8;
 	dstrect.y = 1;
-	dstrect.w = effu_w*6;
-	dstrect.h = effu_h*2;
+	dstrect.w = effu_w * 6;
+	dstrect.h = effu_h * 2;
 	SDL_RenderCopy(ren, house_texture, NULL, &dstrect);
 }
 
@@ -423,15 +662,15 @@ void RenderShirt() {
 			pixelskuvio[y*gw + x] = pixelskuvio[(y*gw) + (gw - 1 - x)];
 		}
 	}
-	
+
 	SDL_UpdateTexture(kuviotexture, NULL, pixelskuvio, 160 * sizeof(Uint32));
 	//SDL_SetTextureAlphaMod(paita_texture, 255);
 	SDL_Rect dstrect;
 
-	dstrect.x = effu_w*1.62;
-	dstrect.y = effu_h*0.9;
+	dstrect.x = effu_w * 1.62;
+	dstrect.y = effu_h * 0.9;
 	dstrect.w = 160 * 4;
-	dstrect.h = 100*2.7;
+	dstrect.h = 100 * 2.7;
 	SDL_RenderCopy(ren, kuviotexture, NULL, &dstrect);
 	SDL_SetTextureBlendMode(paita_texture, SDL_BLENDMODE_BLEND);
 
@@ -487,6 +726,7 @@ int main(int argc, char * argv[]) {
 
 	SDL_SetColorKey(font_image, SDL_TRUE, SDL_MapRGB(font_image->format, 0x0, 0x0, 0x0));
 	SDL_SetColorKey(paita_image, SDL_TRUE, SDL_MapRGB(font_image->format, 0xff, 0xff, 0x0));
+	SDL_SetColorKey(reversecross_image, SDL_TRUE, SDL_MapRGB(reversecross_image->format, 0xff, 0xff, 0xff));
 	reversecross_texture = SDL_CreateTextureFromSurface(ren, reversecross_image);
 	font_texture = SDL_CreateTextureFromSurface(ren, font_image);
 	paita_texture = SDL_CreateTextureFromSurface(ren, paita_image);
@@ -515,9 +755,9 @@ int main(int argc, char * argv[]) {
 	for (int y = 0; y < 15; y++) {
 		for (int x = 0; x < 16; x++) {
 			SDL_Rect rect;
-			rect.x = x * 61-(x>0?1:0);
+			rect.x = x * 61 - (x > 0 ? 1 : 0);
 			rect.y = y * 50;
-			rect.w = 25-(y<5?0:4);
+			rect.w = 25 - (y < 5 ? 0 : 4);
 			rect.h = 32;
 
 			font_bb[(y * 16) + x] = rect;
@@ -553,7 +793,7 @@ int main(int argc, char * argv[]) {
 	clear_a = sync_get_track(rocket, "clear.a");
 
 	cam_rot = sync_get_track(rocket, "cam.rot"),
-	cam_dist = sync_get_track(rocket, "cam.dist");
+		cam_dist = sync_get_track(rocket, "cam.dist");
 	cam_x = sync_get_track(rocket, "cam.x");
 	cam_y = sync_get_track(rocket, "cam.y");
 	cam_h = sync_get_track(rocket, "cam.h");
@@ -609,7 +849,7 @@ int main(int argc, char * argv[]) {
 		sync_rot = float(sync_get_val(cam_rot, row));
 		sync_dist = float(sync_get_val(cam_dist, row));
 
-		time = row*row_rate*4;
+		time = row * row_rate * 4;
 
 		memset(pixels, 0, effu_w * effu_h * sizeof(Uint32));
 		memset(pixelskuvio, 0, 160 * 100 * sizeof(Uint32));
@@ -631,7 +871,15 @@ int main(int argc, char * argv[]) {
 			break;
 		case 3:
 			RenderShirt();
-//			SDL_RenderCopy(ren, scrtexture, NULL, NULL);
+			//			SDL_RenderCopy(ren, scrtexture, NULL, NULL);
+			break;
+		case 4:
+			SphereEffect();
+			SDL_RenderCopy(ren, scrtexture, NULL, NULL);
+			break;
+		case 5:
+			FireEffect();
+			SDL_RenderCopy(ren, scrtexture, NULL, NULL);
 			break;
 		}
 
